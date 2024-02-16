244454b9afb918a826eb14847cb8b65cbc788533
renaisme <huytuduelist@gmail.com>
2024-02-16 09:10:38 +0700

    add copy/move-constructor example

---
+++ Constructor & Destructor/move-constructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class A {
+    int *p;
+    public:
+        A() {
+            std::cout << "Constructor Call" << std::endl;
+        }
+
+        A(int) {
+            std::cout << "Parameterized Constructor" << std::endl;
+        }
+
+        A(A&){
+            std::cout << "Copy cons" << std::endl;
+        }
+
+        A(A&&){ // && - reference to Rvalue
+            std::cout << "Move cons" << std::endl;
+        }
+
+        ~A() {
+            std::cout << "Destructor Call" << std::endl;
+        }
+
+};
+int main() {
+    A a;
+    A b(a); // copy cons
+    A c = std::move(a); // move cons
+    A d = static_cast<A&&>(a); // move cons - not rcm to use 
+    return 0;
+}
