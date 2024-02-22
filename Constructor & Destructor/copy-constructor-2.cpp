62b1d667b195696bf47a768cb8d4503ea43f3574
renaisme <huytuduelist@gmail.com>
2024-02-23 01:27:16 +0700

    copy cons pos compare with move example

---
+++ Constructor & Destructor/copy-constructor-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+class DataWrapper {
+    std::vector<int> data;
+
+    public:
+        DataWrapper(const std::vector<int>& input) : data(input) {}
+
+        // Copy Cons
+        DataWrapper(const DataWrapper& other) : data(other.data) {}
+
+        void addNumber(int num) {
+            data.push_back(num);
+        }
+
+        void printData() const {
+            for(int num : data) {
+                std::cout << num << " ";
+            }
+            std::cout << std::endl;
+        }
+};
+
+void processData(DataWrapper data) {
+    data.addNumber(100);
+    std::cout << "Data in processData: ";
+    data.printData();
+}
+
+int main() {
+    std::vector<int> initialData = {1, 2, 3, 4, 5};
+    DataWrapper originalData(initialData);
+
+    processData(originalData);
+
+    std::cout << "Data after process: ";
+    originalData.printData();
+    
+    return 0;
+}
