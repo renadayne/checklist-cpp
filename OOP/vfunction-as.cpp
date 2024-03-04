a33143ffa6cce2999176e2ca90db1508a5032b41
renaisme <huytuduelist@gmail.com>
2024-03-04 22:51:04 +0700

    virtual function in data segment

---
+++ OOP/vfunction-as.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+class A{
+    virtual void bye();
+    virtual void hello();
+    virtual void helo();
+}B;
+ 
+void A::bye(){
+};
+void A::hello(){
+};
+void A::helo(){
+};
+ 
+int main() {
+  return 0;
+}
